cef7938849b2bca60a6147d6d9c71257d1019718
Jonathan Pérez <jopcodecl@gmail.com>
2017-05-06 02:45:51 +0200

    Change colors of tetrminos to images

--- src/tetris/ControlPrincipal.java
+++ src/tetris/ControlPrincipal.java
@@ -32,9 +32,8 @@ public class ControlPrincipal {
     }
     
     public static void iterarBuclePrincipal() {
-        long millis = System.currentTimeMillis();
         final int NS_POR_SEGUNDO = 1000000000;
-        final int APS_OBJETIVO = 60;
+        final int APS_OBJETIVO = 120;
         final int NS_POR_ACTUALIZACION = NS_POR_SEGUNDO / APS_OBJETIVO;
         
         long tiempoDeReferenciaActualizacion = System.nanoTime(); 
@@ -53,7 +52,7 @@ public class ControlPrincipal {
                 delta--;
                 actualizar();
             }
-            
+         
             dibujar();
            
             if(System.nanoTime() - tiempoDeReferenciaContador > NS_POR_SEGUNDO) {
--- src/tetris/Imagen.java
+++ src/tetris/Imagen.java
@@ -1,13 +1,14 @@
 package tetris;
-
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
 
 public class Imagen implements InformacionGeneral{
-    
+     
     SuperficieDeDibujo superficieDeDibujo;
     Dupla posicionTablero;
- 
+    
     
     public Imagen(SuperficieDeDibujo superficieDeDibujo) {
         this.superficieDeDibujo = superficieDeDibujo;
@@ -20,33 +21,48 @@ public class Imagen implements InformacionGeneral{
         return new Dupla(X, Y);
     }
     
-    public void dibujarPeriferico(Dupla pos, Graphics g, int tipo) {
-        Color color;
-        switch(tipo){
-            case 0:
-                g.setColor(new Color(228, 0, 39));
-                break;
-            case 1:
-                g.setColor(new Color(0, 228, 39));
-                break;
-            case 2:
-                g.setColor(new Color(0, 78, 228));
-                break;
-            case 3:
-                g.setColor(new Color(228, 98, 0));
-                break;
-            case 4:
-                g.setColor(new Color(158,19, 228));
-                break;
-            case 5:
-                g.setColor(new Color(228, 222, 0));
-                break;
-            case 6:
-                g.setColor(new Color(0, 228, 228));
-                break;
+    public void dibujarPeriferico(Dupla pos, Graphics g, int nombre) {
+        
+        g.drawImage(cargarImagen(), calcularPosicion(pos).intX(), calcularPosicion(pos).intY(), superficieDeDibujo);
+            
+//        switch(tipo){
+//            case 0:
+//                g.setColor(new Color(228, 0, 39));
+//                break;
+//            case 1:
+//                g.setColor(new Color(0, 228, 39));
+//                break;
+//            case 2:
+//                g.setColor(new Color(0, 78, 228));
+//                break;
+//            case 3:
+//                g.setColor(new Color(228, 98, 0));
+//                break;
+//            case 4:
+//                g.setColor(new Color(158,19, 228));
+//                break;
+//            case 5:
+//                g.setColor(new Color(228, 222, 0));
+//                break;
+//            case 6:
+//                g.setColor(new Color(0, 228, 228));
+//                break;
+//        }
+        
+
+    }
+    
+    public BufferedImage cargarImagen() {
+      
+        BufferedImage imagen = null;
+        
+        try {
+            imagen = ImageIO.read(ClassLoader.class.getResource("/images/tetrimino.png"));
+            //= ImageIO.read(ClassLoader.class.getResource("images/mino.png"));
+        } catch (Exception e) {
+            System.out.println("Error cargando imagen!" + e);
         }
-        g.fillRect((int)calcularPosicion(pos).X, (int)calcularPosicion(pos).Y, ANCHO_TETRIMINO, ALTO_TETRIMINO);
-        g.setColor(Color.BLACK);
-        g.drawRect((int)calcularPosicion(pos).X, (int)calcularPosicion(pos).Y, ANCHO_TETRIMINO, ALTO_TETRIMINO);
+        
+        return imagen;
     }
 }
